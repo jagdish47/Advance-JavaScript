12d43d7597b3a124eeabe012cb6b72694fa1dbf6
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-08-03 00:23:03 +0530

    Day - 1

--- Day - 1/index.js
+++ Day - 1/index.js
@@ -3,3 +3,39 @@ console.log(typeof(null));
 
 console.log(1 == "1");
 // true
+
+console.log(1 === "1");
+//false
+
+
+
+// Create Null Object
+
+var obj = {};
+
+var obj = null;
+
+
+
+
+
+var n; //Initialization/Declaration
+var n = "jassi kumawat"; //Assignment
+
+var n = "Jagdish Kumawat"; //Re-Assignment
+
+
+
+
+
+
+
+
+
+var arr = [5, 7, 4];
+arr[0] = 100;  //can change array is mutable.
+console.log(arr);
+
+var str = "Jagdish";
+str[0] = "K"; //won't change becuase we can't change string its immutable
+console.log(str);  
